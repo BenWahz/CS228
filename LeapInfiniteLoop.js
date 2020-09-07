372ab5512e7f30abe9293e845cd7e6e80e295389
Henry Rice <hmrice@uvm.edu>
2020-09-07 15:18:50 -0400

    Code is now extracting fingers data to the console log

--- LeapInfiniteLoop.js
+++ LeapInfiniteLoop.js
@@ -1,7 +1,22 @@
 // JavaScript source code
 var controllerOptions = {};
+var x = (window.innerWidth)/2;
+var y = (window.innerHeight)/2;
 Leap.loop(controllerOptions, function(frame)
 {
-console.log(i)
+	//clear();
+	//var rand1 = (Math.floor(Math.random()*5)-5); //Had to make values bigger because you could not see the movement from '1'
+	//var rand2 = (Math.floor(Math.random()*5)-5);
+	//circle(x+rand1,y+rand2,100)
+
+	
+	
+	if (frame.hands.length == 1){
+		var hand = frame.hands[0];
+		var fingers = hand.fingers;
+				
+
+		console.log(fingers);
+	}
 }
 );
