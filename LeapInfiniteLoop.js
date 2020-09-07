69b42f81cd0a74379fd81394a4265317eff71efa
Henry Rice <hmrice@uvm.edu>
2020-09-07 16:39:19 -0400

    Circle is moving in relation to finger tip

--- LeapInfiniteLoop.js
+++ LeapInfiniteLoop.js
@@ -1,27 +1,49 @@
 // JavaScript source code
 var controllerOptions = {};
-var x = (window.innerWidth)/2;
-var y = (window.innerHeight)/2;
+var x = 0;
+var y = 0;
+var z = 0;
+
 Leap.loop(controllerOptions, function(frame)
 {
-	//clear();
+	clear();
 	//var rand1 = (Math.floor(Math.random()*5)-5); //Had to make values bigger because you could not see the movement from '1'
 	//var rand2 = (Math.floor(Math.random()*5)-5);
-	//circle(x+rand1,y+rand2,100)
+	
 
-	
-	
-	if (frame.hands.length == 1){
-		var hand = frame.hands[0];
-		var fingers = hand.fingers;
-				
-		for(var i = 0; i < hand.fingers.length; i++){
-			finger = hand.fingers[i];
-		
-			if (finger.type == 1){
-				console.log(finger);
-			}
-		}
-	}
+	HandleFrame(frame);
+	circle(x+((window.innerWidth)/2), (z)*(-1)+((window.innerHeight)),50)
 }
 );
+
+
+
+function HandleFrame(frame)
+{
+
+	if (frame.hands.length == 1)
+	{
+		var hand = frame.hands[0];
+		HandleHand(hand)
+	}
+}
+
+function HandleHand(hand)
+{
+	for(var i = 0; i < hand.fingers.length; i++)
+	{
+		finger = hand.fingers[i];
+		HandleFinger(finger);
+	}
+}
+
+function HandleFinger(finger)
+{
+	if (finger.type == 1)
+	{
+		x = finger.tipPosition[0];
+		y = finger.tipPosition[2];
+		z = finger.tipPosition[1];
+		console.log(finger.tipPosition);
+	}
+}
