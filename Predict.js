8b89b47e430fed4e0ba7e048b12a8266e1d8318b
Henry Rice <hmrice@uvm.edu>
2020-09-28 18:30:35 -0400

    updating Predict.js

--- Predict.js
+++ Predict.js
@@ -152,11 +152,13 @@ var irisData = nj.array([[	5.1	,	3.5	,	1.4	,	0.2	],
 [	5.9	,	3	,	5.1	,	1.8	]]);
 
 var numSamples = irisData.shape[0];
-var numFeatures = irisData.shape[1]-1;
+var numFeatures = (irisData.shape[1])-1;
 
-var testingSampleIndex = 1;
+var testingSampleIndex = 0;
 
 var trainingCompleted = false;
+
+var predictedClassLabels = nj.zeros([numSamples]);
 
 function draw()
 {
@@ -170,7 +172,7 @@ function draw()
     }
 
     Test();
-
+    drawCircles();
     // console.log(irisData.toString());
     // console.log(numSamples);
     // console.log(numFeatures);
@@ -178,12 +180,16 @@ function draw()
 
 function GotResults(err, result)
 {
-    console.log(testingSampleIndex + 1, parseInt(result.label));
+    //console.log(testingSampleIndex + 1, parseInt(result.label));
+
     testingSampleIndex += 2;
     if (testingSampleIndex > numSamples)
     {
         testingSampleIndex = 1;
     }
+
+    predictedClassLabels[testingSampleIndex] = parseInt(result.label);
+
 }
 
 function Train()
@@ -193,10 +199,10 @@ function Train()
     for(i = 0; i < numSamples; i+=2)
     {
 
-        var currentFeatures = irisData.pick(i).slice([0,4]);
+        var currentFeatures = irisData.pick(i).slice([0,2]);
         var currentLabel = irisData.get(i,-1); //classLabel
         knnClassifier.addExample(currentFeatures.tolist(), currentLabel);
-        console.log(i, currentFeatures.toString(), currentLabel);
+        //console.log(i, currentFeatures.toString(), currentLabel);
     }
     trainingCompleted = true;
 }
@@ -205,7 +211,7 @@ function Test()
 {
     console.log("I am testing");
 
-    var currentFeatures = irisData.pick(testingSampleIndex).slice([0,4]);
+    var currentFeatures = irisData.pick(testingSampleIndex).slice([0,2]);
     var currentLabel = irisData.get(testingSampleIndex,-1); //classLabel
     var predictedLabel = knnClassifier.classify(currentFeatures.tolist(), GotResults);
     //console.log(currentFeatures.toString(), currentLabel);
@@ -221,4 +227,53 @@ function Test()
 
 }
 
+
+function drawCircles()
+{
+    for(i = 0; i < numSamples; i++)
+    {
+        var x = irisData.get(i,0);
+        var y = irisData.get(i,1);
+        var c = irisData.get(i,-1);
+        c = parseInt(c);
+        console.log(c);
+
+        if(i % 2 == 0)
+        {
+            //outline
+            stroke(51);
+        }else
+        {
+            //outline
+            if(c == 0)
+            {
+                stroke('rgb(255,0,0)');
+            }else if (c == 1)
+            {
+                stroke('rgb(0,0,255)');
+            }else if (c == 2)
+            {
+                stroke('rgb(0,255,0)');
+            }
+        }
+        //interior
+        if(c == 0)
+        {
+            fill('rgb(255,0,0)');
+        }else if (c == 1)
+        {
+            fill('rgb(0,0,255)');
+        }else if (c == 2)
+        {
+            fill('rgb(0,255,0)');
+        }
+
+
+
+
+
+        //circle(x*100,y*100,8);
+        console.log(predictedClassLabels);
+    }
+}
 //draw();
